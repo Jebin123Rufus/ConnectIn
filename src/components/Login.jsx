50d6657e6447fcdd4c4bc458a7b986077cfec23d
unknown <jebinrufuz@gmail.com>
2025-11-23 18:52:13 +0530

    Styled login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,24 @@
 import { GoogleLogin } from '@react-oauth/google';
-import { jwtDecode } from 'jwt-decode';
 import { useNavigate } from 'react-router-dom';
+import './Login.css';
 
-function Login() {
-
+export default function Login() {
+  
   const navigate = useNavigate();
 
   return (
     <>
+      <div className='auth-card'>
+        <h2>Welcome  To  ConnectIn 👋</h2>
+        <p>Sign in to continue</p>
         <GoogleLogin onSuccess={(credentialResponse) => {
-            console.log(credentialResponse);
-            console.log(jwtDecode(credentialResponse.credential));
             navigate('/landing');
-        }} onError={() => console.error("Login Failed!!")}/>
+        }} onError={() => console.error("Login Failed!!")}
+        size="large"
+        shape="rectangular"
+        text="continue_with"
+        />
+      </div>
     </>
   )
 }
-
-export default Login
