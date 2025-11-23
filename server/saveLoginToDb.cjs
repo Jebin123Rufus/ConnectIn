edd79592aaa918d015b9d682d4de1b49ba23bdd7
unknown <jebinrufuz@gmail.com>
2025-11-23 21:32:22 +0530

    Login Authentication is stored on Database

---
+++ server/saveLoginToDb.cjs
@@ -0,0 +1,28 @@
+const { MongoClient } = require("mongodb");
+require("dotenv").config({ path: "./config.env" });
+
+const Db = process.env.MONGODB_URI;
+const client = new MongoClient(Db);
+
+async function saveLoginToDB(userData) {
+  try {
+    const database = client.db("app");
+    const loginsCollection = database.collection("Logins");
+
+    const existingUser = await loginsCollection.findOne({ email: userData.email });
+
+    if (existingUser) {
+      console.log(`User with email ${userData.email} already exists.`);
+      return { status: "exists", user: existingUser };
+    }
+
+    const result = await loginsCollection.insertOne(userData);
+    return { status: "inserted", userId: result.insertedId };
+
+  } catch (err) {
+    console.error("Error saving login:", err);
+    return { status: "error", error: err };
+  }
+}
+
+module.exports = saveLoginToDB;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,19 +1,34 @@
 import { GoogleLogin } from '@react-oauth/google';
 import { useNavigate } from 'react-router-dom';
+import { jwtDecode } from 'jwt-decode';
+import axios from 'axios';
 import './Login.css';
 
 export default function Login() {
   
   const navigate = useNavigate();
 
+  const handleLogin = async (credentialResponse) => {
+    try {
+
+      const decoded = jwtDecode(credentialResponse.credential);
+
+      const res = await axios.post("http://localhost:5000/auth/google", decoded);
+
+      localStorage.setItem("user", JSON.stringify(res.data.user));
+      navigate("/landing");
+
+    } catch (err) {
+      console.error("Login Failed:", err);
+    }
+  };
+
   return (
     <>
       <div className='auth-card'>
         <h2>Welcome  To  ConnectIn 👋</h2>
         <p>Sign in to continue</p>
-        <GoogleLogin onSuccess={(credentialResponse) => {
-            navigate('/landing');
-        }} onError={() => console.error("Login Failed!!")}
+        <GoogleLogin onSuccess={handleLogin} onError={() => console.error("Login Failed!!")}
         size="large"
         shape="rectangular"
         text="continue_with"
