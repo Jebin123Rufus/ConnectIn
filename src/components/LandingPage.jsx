8d1d1a03f870f0ecb14bbee5cf934da0439de3e0
JoshuaDaniell <joshdanie05@gmail.com>
2025-11-24 22:16:24 +0530

    changes and navigation added

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,11 +1,23 @@
 import { useNavigate } from 'react-router-dom';
+import { CgProfile } from "react-icons/cg";
+import { CgOrganisation } from "react-icons/cg";
+import './LandingPage.css';
 function LandingPage() {
   const navigate = useNavigate();
   return (
     <>
         <h1>ConnectIn</h1>
-        <button onClick={() => {navigate("/individual")}}>Individual</button>
-        <button onClick={() => { navigate("/organisation")}}>Organisation</button>
+        <h4> Make Your first move By Choosing Your Role </h4>
+        <div class = "buttons">
+          <button onClick={() => {navigate("/individual")}}>
+            <CgProfile className="identity-icon" /> Individual
+          </button>
+          <button onClick={() => { navigate("/organisation")}}>
+            <CgOrganisation className= "identity-icon" /> 
+            Organisation
+          </button>
+        </div>
+        
         <h1>Choose Your Identity !</h1>
     </>
   )
